c1abfec2cec98b063413c76d1e94ff1a15d4c32f
lorobert <lorobert@student.42lausanne.ch>
2025-07-29 14:36:06 +0200

    Fix active link & better link management

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import LogoImage from '../../public/logo.jpg';
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
@@ -12,6 +13,17 @@ export default function Header() {
     const toggleHamburger = () => {
         setHamburgerOpen(!hamburgerOpen);
     }
+
+    const NAV_ITEMS = [
+        { href: "/", label: "Home" },
+        { href: "/presentation", label: "Présentation" },
+        { href: "/cours", label: "Cours" },
+        { href: "/ballade", label: "Ballade active" },
+        { href: "/ressources", label: "Ressources" },
+        { href: "/trouver", label: "Nous trouver" },
+        { href: "/contact", label: "Contact" },
+    ];
+    const pathname = usePathname();
 
     return (
         <header className="flex flex-col justify-center items-center basis-full">
@@ -22,13 +34,15 @@ export default function Header() {
                     <X size={42} className={`${hamburgerOpen ? '' : 'hidden'}`} />
                 </button>
                 <ul className={`${hamburgerOpen ? 'flex w-9/10' : 'hidden sm:flex'} flex-col sm:flex-row rounded-md bg-white sm:bg-inherit`}>
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Home</Link>
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Présentation</Link>
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Cours</Link >
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Ballade active</Link>
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Ressources</Link>
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Nous trouver</Link>
-                    <Link className="text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white active:bg-primary" href="/">Contact</Link>
+                    {NAV_ITEMS.map(({ href, label }) => {
+                        return (
+                            <Link
+                                key={href}
+                                className={`${pathname === href ? 'active bg-primary' : ''} text-2xl px-4 sm:py-4 mx-0 rounded-md hover:bg-white`}
+                                href={href}
+                            >{label}</Link>
+                        );
+                    })}
                 </ul>
             </nav>
             <hr className="w-4/5 h-px my-4 bg-primary border-0" />
